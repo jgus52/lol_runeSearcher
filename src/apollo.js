c660c0763d877c89eb7e9f79a8cc3cf7f269655f
unknown <srraight@gmail.com>
2022-02-04 17:49:53 +0900

    edit server url for riot.txt

--- src/apollo.js
+++ src/apollo.js
@@ -3,7 +3,7 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 export const client = new ApolloClient({
   uri:
     process.env.NODE_ENV === "production"
-      ? "https://lol-runesearcher-kr.herokuapp.com/"
+      ? "https://lol-runesearcher-kr.herokuapp.com/graphql"
       : "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
